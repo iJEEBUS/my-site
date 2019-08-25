470ffaa6cb08cac8d28a41c3f61598e060756edc
Ron Rounsifer <rounsifr@mail.gvsu.edu>
2019-08-25 17:44:38 -0400

    Updates

---
+++ precache-manifest.1c856e6d7a4f8b6770d80b50a6eaf7e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429a8a1188bcd6752325d0c43b81c9b0",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "89bd27fe20ffde358b26",
+    "url": "/my-site/static/css/main.c0622627.chunk.css"
+  },
+  {
+    "revision": "cca8334be3193730cbbd",
+    "url": "/my-site/static/js/2.bf879f72.chunk.js"
+  },
+  {
+    "revision": "89bd27fe20ffde358b26",
+    "url": "/my-site/static/js/main.9c392e34.chunk.js"
+  },
+  {
+    "revision": "afcaf2abaebe648291ef",
+    "url": "/my-site/static/js/runtime~main.cb3228ac.js"
+  }
+]);
